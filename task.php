6d8f29c763ba2262e6e74b599061caef02a96d4d
Mosfiqur Rahman Ome <93466940+MusfiqOmi@users.noreply.github.com>
2022-06-07 13:53:50 +0600

    Add files via upload

---
+++ task.php
@@ -0,0 +1,13 @@
+<?php
+
+function area($length =6, $width =4) 
+{
+    
+    $area = $length * $width;
+    echo "Area Of Rectangle : "  . $area  . "<br>" ;
+    $perimeter = 2 * ($length + $width);
+    echo "Perimeter : ".$perimeter . "<br>";
+}
+area(); 
+
+?>
